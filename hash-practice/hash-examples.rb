101fdfe8bf37644d50c9e4bd8569025463bb2182
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-20 22:25:16 -0400

    Create animal hash

--- hash-practice/hash-examples.rb
+++ hash-practice/hash-examples.rb
@@ -2,3 +2,5 @@
 my_collection {car: "Mercedes", animal: "Dog", customer: "None"}
 
 car {model: "A220", color: "black", miles: 2100}
+
+animal {type: "Dog", breed: "Retriever", is_male: True, age: 6}
