dbbaead149e73bbcbf7c42ad53e603623cc17a4d
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-20 22:27:43 -0400

    Create customer model

--- hash-practice/hash-examples.rb
+++ hash-practice/hash-examples.rb
@@ -4,3 +4,5 @@ my_collection {car: "Mercedes", animal: "Dog", customer: "None"}
 car {model: "A220", color: "black", miles: 2100}
 
 animal {type: "Dog", breed: "Retriever", is_male: True, age: 6}
+
+customer {is_buying: true, been_approached: false, name: "Buyer"}
