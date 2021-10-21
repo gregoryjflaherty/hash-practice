32729dffe76fa3380743d945e8634cfc9c9378cb
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-21 11:33:31 -0400

    Fix syntax errors

--- hash-practice/hash-examples.rb
+++ hash-practice/hash-examples.rb
@@ -1,8 +1,15 @@
 # hash examples
-my_collection {car: "Mercedes", animal: "Dog", customer: "None"}
+my_collection = {car: "Mercedes", animal: "Dog", customer: "None"}
 
-car {model: "A220", color: "black", miles: 2100}
+car = {model: "A220", color: "black", miles: 2100}
 
-animal {type: "Dog", breed: "Retriever", is_male: True, age: 6}
+animal = {type: "Dog", breed: "Retriever", is_male: true, age: 6}
 
-customer {is_buying: true, been_approached: false, name: "Buyer"}
+customer = {is_buying: true, been_approached: false, name: "Buyer"}
+
+portfolio = {grt: 500, algo: 1000, matic: 5, binance: ["cardano", "harmony", "cosmos"]}
+puts portfolio[:grt]
+puts portfolio[:binance][0..]
+puts car[:color]
+sell_now = portfolio[:matic]
+puts sell_now
